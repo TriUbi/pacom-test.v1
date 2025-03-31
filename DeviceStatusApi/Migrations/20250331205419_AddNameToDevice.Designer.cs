9eb75a01d8f18a0a721c0b4b65a8ec17284f8128
TriUbi <triubilla@gmail.com>
2025-03-31 22:54:58 +0200

    lägger till  AddNameToDevice i db

--- DeviceStatusApi/Migrations/20250331205419_AddNameToDevice.Designer.cs
+++ DeviceStatusApi/Migrations/20250331205419_AddNameToDevice.Designer.cs
@@ -2,6 +2,7 @@
 using DeviceStatusApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DeviceStatusApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331205419_AddNameToDevice")]
+    partial class AddNameToDevice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
