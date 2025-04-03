f472595596f353206c57c5075574da2162aacbd3
TriUbi <triubilla@gmail.com>
2025-04-03 22:00:19 +0200

    modbus controller fix and test, evrything ok

--- DeviceStatusApi/Controllers/ModbusController.cs
+++ DeviceStatusApi/Controllers/ModbusController.cs
@@ -4,19 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class ModbusController : ControllerBase
 {
+    // POST: api/modbus/set?coilAddress=1&value=true
     [HttpPost("set")]
-    public IActionResult Set([FromQuery] bool value)
+    public IActionResult Set([FromQuery] int coilAddress, [FromQuery] bool value)
     {
         using var modbus = new ModbusService();
-        modbus.WriteCoilStatus(1, 0, value); 
-        return Ok($"Value set to {value}");
+        modbus.WriteCoilStatus(1, (ushort)coilAddress, value);
+        return Ok($"Coil {coilAddress} set to {value}");
     }
 
+    // GET: api/modbus/status?coilAddress=1
     [HttpGet("status")]
-    public IActionResult GetStatus()
+    public IActionResult GetStatus([FromQuery] int coilAddress)
     {
         using var modbus = new ModbusService();
-        var status = modbus.ReadCoilStatus(1, 0);
-        return Ok(status);
+        var status = modbus.ReadCoilStatus(1, (ushort)coilAddress);
+        return Ok($"Coil {coilAddress} is {(status ? "ON" : "OFF")}");
     }
 }
